990db69708fb9538ae8455aaf30bc8933e9255ad
Dan Jarrett <djarrett@newlifeinfo.com>
2018-03-05 13:04:35 -0800

    Initial Commit

---
+++ First/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  First
+//
+//  Created by Dan Jarrett on 3/4/18.
+//  Copyright © 2018 Dan Jarrett. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ First/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by Dan Jarrett on 3/4/18.
+//  Copyright © 2018 Dan Jarrett. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
